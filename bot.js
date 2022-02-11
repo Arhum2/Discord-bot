5d3a622902b8621f051c0b25589f3d5888e4bda8
Arhum2 <Arhumshahzad2003@gmail.com>
2022-02-10 23:26:33 -0500

    added intents

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ console.log('beep beep');
 
 const Discord = require('discord.js');
 const { Client, Intents } = require('discord.js');
-const client = new Client({ ws: { intents: Intents.ALL } });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 client.login('OTQxMjExMzMwMTY4MzY5MTYy.YgSpWw.ZzN3cLzKgKA_r0OfCvW4Cf2htTs');
 
 client.on('ready', readyDiscord);
