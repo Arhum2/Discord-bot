e901e5d04c456540f6bd0beafae4d9a7697b9ba9
Arhum2 <arhumshahzad2003@gmail.com>
2022-02-10 22:35:18 -0500

    first commit

---
+++ bot.js
@@ -0,0 +1,18 @@
+console.log('beep beep');
+
+const Discord = require('discord.js');
+const { Client, Intents } = require('discord.js');
+const client = new Client({ ws: { intents: Intents.ALL } });
+client.login('OTQxMjExMzMwMTY4MzY5MTYy.YgSpWw.ZzN3cLzKgKA_r0OfCvW4Cf2htTs');
+
+client.on('ready', readyDiscord);
+
+function readyDiscord() {
+    console.log('heart');
+}
+
+client.on('message', gotMessage);
+
+function gotMessage(msg) {
+    console.log(msg.content);
+}
