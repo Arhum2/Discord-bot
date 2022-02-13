8df52d9ab2d2c911b54d5ae76b7a697a9a97d08a
Arhum2 <arhumshahzad2003@gmail.com>
2022-02-12 20:10:34 -0500

    Added node fetch

--- bot.js
+++ bot.js
@@ -1,23 +1,28 @@
 console.log('beep beep');
 
 require('dotenv').config();
-const Discord = require('discord.js');
+
+const fetch = import('node-fetch');
 const { Client, Intents } = require('discord.js');
 
 const client = new Client({ intents: ['GUILDS', 'GUILD_MESSAGES'] });
 client.login(process.env.BOTTOKEN);
 
-client.on('ready', readyDiscord);
-
-function readyDiscord() {
-    console.log('heart');
-}
 
 client.on('message', gotMessage);
 
-function gotMessage(msg) {
+async function gotMessage(msg) {
     console.log(msg.content);
     if (msg.content === 'choo choo') {
         msg.reply("Train");
+    } else if (msg.content == '!hero') {
+        msg.channel.send('gif!');
+
+        let url = `https://best-overwatch-api.herokuapp.com/`
+
+        let response = await fetch(url);
+        let json = await response.json();
+        console.log(json);
+
     }
 }
