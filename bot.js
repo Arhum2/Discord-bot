ebb21c987ef9201788cb3b09b485f028643ad7fc
Arhum2 <arhumshahzad2003@gmail.com>
2022-02-26 02:10:32 -0500

    console logs username

--- bot.js
+++ bot.js
@@ -13,11 +13,11 @@ client.on('messageCreate', gotMessage);
 async function gotMessage(msg) {
     console.log(msg.content)
     if (msg.content === '!stats') {
-        axios.get('https://best-overwatch-api.herokuapp.com/player/pc/us/xxxxxxxx-1494')
+        axios.get('https://best-overwatch-api.herokuapp.com/player/pc/us/JustArhum-1494')
         
             .then((res) => {
                 msg.reply('working')
-                var stringify = (JSON.stringify(res));
+                var stringify = (JSON.stringify(res.data));
                 var parseData = JSON.parse(stringify);
                 console.log('stringify:', stringify);
                 console.log('parseData:', parseData);
