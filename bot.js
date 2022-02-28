ab112571e2e303d85ee365a198994acee87afe46
Arhum2 <arhumshahzad2003@gmail.com>
2022-02-28 01:39:26 -0500

    bot responds with user's level and username

--- bot.js
+++ bot.js
@@ -1,46 +1,50 @@
-console.log('Online');
+console.log("Online");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const axios = require('axios')
-const { Client, Intents, Message } = require('discord.js');
-const client = new Client({ intents: ['GUILDS', 'GUILD_MESSAGES'] });
+const axios = require("axios");
+const { Client, Intents, Message } = require("discord.js");
+const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 client.login(process.env.BOTTOKEN);
 
-
-client.on('messageCreate', gotMessage);
+client.on("messageCreate", gotMessage);
 
 async function gotMessage(msg) {
-    console.log(msg.content)
-    if (msg.content.startsWith('!stats')) {
-        //taking user input and concatinating it with the API base url to feth stats
-        let url = "https://best-overwatch-api.herokuapp.com/player/";
+  console.log(msg.content);
+  if (msg.content.startsWith("!stats")) {
+    //taking user input and concatinating it with the API base url to feth stats
+    let url = "https://best-overwatch-api.herokuapp.com/player/";
 
-        let text = msg.content
-        //console.log('TEXT:', text)
-        
-        let str = text.replace("!stats", "")
-        //console.log('STR:', str)
+    let text = msg.content;
+    //console.log('TEXT:', text)
 
-        playerDetails = str.trim()
-        //console.log('PLAYERDETAILS:', playerDetails)
-        
-        let result = url.concat(playerDetails)
-        //console.log('RESULT:', result)
-        
-        axios.get(result)
-        
-            .then((res) => {
-                msg.reply('working')
-                var stringify = (JSON.stringify(res.data));
-                var parseData = JSON.parse(stringify);
-                console.log('stringify:', stringify);
-                console.log('parseData:', parseData);
-                console.log(parseData.username);
+    let str = text.replace("!stats", "");
+    //console.log('STR:', str)
 
-            })
-            .catch((err) => {
-            console.log("ERR:", err)
-            })
-    }
+    playerDetails = str.trim();
+    //console.log('PLAYERDETAILS:', playerDetails)
+
+    let result = url.concat(playerDetails);
+    //console.log('RESULT:', result)
+
+    axios
+      .get(result)
+
+      .then((res) => {
+        var stringify = JSON.stringify(res.data);
+        var parseData = JSON.parse(stringify);
+        console.log("stringify:", stringify);
+        console.log("parseData:", parseData);
+        console.log(parseData.username);
+        msg.reply(`
+        Level: ${parseData.level} 
+        Username: ${parseData.username}
+        `)
+      })
+
+
+      .catch((err) => {
+        console.log("ERR:", err);
+      });
+  }
 }
